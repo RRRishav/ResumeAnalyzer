2d308d635544f575d93bbca5c3e3fbd3c222723d
Navneet-brar <115395844+Navneet-brar@users.noreply.github.com>
2026-05-14 12:28:58 +0530

    updated CometEffect

--- client/src/components/CometEffect.jsx
+++ client/src/components/CometEffect.jsx
@@ -1,6 +1,3 @@
-// This component is no longer used in the professional theme
-// Keeping it for reference if needed in the future
-
 import React from 'react';
 
 export default function CometEffect() {
